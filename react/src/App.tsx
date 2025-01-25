94bcf5eddc19e0dcc56ddf3eea5bd51b1826f631
woojung007 <eeeeve007@gmail.com>
2025-01-26 00:18:21 +0900

    🐛 Fix: css typescript

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
+import CssPage from './react-css/CssPage';
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,7 @@ const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={true} />
+            <CssPage />
         </QueryClientProvider>
     );
 };

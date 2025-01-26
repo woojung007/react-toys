35023601d6a8486ed1fe91e63877ec9268d97652
woojung007 <eeeeve007@gmail.com>
2025-01-26 17:56:18 +0900

    ✨ UseRef로 컴포넌트의 변수 생성하기

--- react/src/one-bite/Register.tsx
+++ react/src/one-bite/Register.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 
 export default function Register() {
     const [input, setInput] = useState({
@@ -7,23 +7,55 @@ export default function Register() {
         country: '',
         bio: '',
     });
+    const countRef = useRef(0);
+    const registerRef = useRef(0);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const onChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        registerRef.current++;
+        console.log('registerRef.current:', registerRef.current);
         setInput({
             ...input,
             [e.target.name]: e.target.value,
         });
     };
+
+    const onSubmit = () => {
+        if (!input.name) {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current?.focus();
+        }
+    };
     return (
         <div>
-            <input onChange={onChange} name='name' type='text' value={input.name} placeholder='이름' />
-            <input onChange={onChange} name='birth' value={input.birth} type='date' />
-            <select onChange={onChange} name='country' value={input.country}>
-                <option value='korea'>한국</option>
-                <option value='japan'>일본</option>
-                <option value='china'>중국</option>
-            </select>
-            <textarea onChange={onChange} name='bio' value={input.bio} />
+            <button
+                onClick={() => {
+                    countRef.current++;
+                    console.log('countRef.current:', countRef.current);
+                }}
+            >
+                ref+1
+            </button>
+
+            <div>
+                <input
+                    ref={inputRef}
+                    onChange={onChange}
+                    name='name'
+                    type='text'
+                    value={input.name}
+                    placeholder='이름'
+                />
+                <input onChange={onChange} name='birth' value={input.birth} type='date' />
+                <select onChange={onChange} name='country' value={input.country}>
+                    <option value='korea'>한국</option>
+                    <option value='japan'>일본</option>
+                    <option value='china'>중국</option>
+                </select>
+                <textarea onChange={onChange} name='bio' value={input.bio} />
+            </div>
+
+            <button onClick={onSubmit}>제출</button>
         </div>
     );
 }

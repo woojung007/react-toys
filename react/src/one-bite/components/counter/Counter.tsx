566a6e2583d1297bc490742155c2961cb2022792
woojung007 <eeeeve007@gmail.com>
2025-01-31 17:20:55 +0900

    ✨ 7.2 useEffect 사용하기

--- react/src/one-bite/components/counter/Counter.tsx
+++ react/src/one-bite/components/counter/Counter.tsx
@@ -1,17 +1,26 @@
 import CounterController from 'one-bite/components/counter/CounterController';
 import CounterViewer from 'one-bite/components/counter/CounterViewer';
 import styles from './Counter.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+    const [input, setInput] = useState('');
 
     const onClickButton = (value: number) => {
         setCount((prev) => prev + value);
     };
+
+    useEffect(() => {
+        console.log('count:', count, 'input:', input);
+    }, [count, input]); // 의존성 배열, dependency array, deps
     return (
         <div className={styles.counter_app}>
             <h1>Simple Counter</h1>
+
+            <section>
+                <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+            </section>
 
             <section>
                 <CounterViewer count={count} />

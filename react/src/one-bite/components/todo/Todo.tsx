7a8dbf64053a699ffc40d017472472bb925bb9a1
woojung007 <eeeeve007@gmail.com>
2025-02-03 21:45:17 +0900

    ✨ 8.3) 기능구현 준비하기

--- react/src/one-bite/components/todo/Todo.tsx
+++ react/src/one-bite/components/todo/Todo.tsx
@@ -2,8 +2,31 @@ import TodoEditor from 'one-bite/components/todo/TodoEditor';
 import TodoHeader from 'one-bite/components/todo/TodoHeader';
 import TodoList from 'one-bite/components/todo/TodoList';
 import styles from './Todo.module.scss';
+import { useState } from 'react';
+
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: 'React 공부하기',
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: '빨래하기',
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: '노래 연습하기',
+        date: new Date().getTime(),
+    },
+];
 
 export default function Todo() {
+    const [todos, setTodos] = useState(mockData);
     return (
         <div className={styles.todo_app}>
             <TodoHeader />

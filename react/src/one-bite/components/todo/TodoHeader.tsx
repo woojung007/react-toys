e123880c6143d75b68ac22518d54bd2fd0208968
woojung007 <eeeeve007@gmail.com>
2025-02-22 12:22:17 +0900

    ➕ Add i18n

--- react/src/one-bite/components/todo/TodoHeader.tsx
+++ react/src/one-bite/components/todo/TodoHeader.tsx
@@ -1,9 +1,10 @@
+import { t } from 'i18next';
 import styles from './TodoHeader.module.scss';
 
 export default function TodoHeader() {
     return (
         <div className={styles.todo_header}>
-            <h3>오늘은 🗓️</h3>
+            <h3>{t(`hello`)} 🗓️</h3>
             <h1>{new Date().toDateString()}</h1>
         </div>
     );

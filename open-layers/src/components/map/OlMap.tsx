4240965a2680bc5dabf93f4e17af1b653c1ec900
woojung007 <eeeeve007@gmail.com>
2025-03-02 17:50:17 +0900

    ✨ Osm tile

--- open-layers/src/components/map/OlMap.tsx
+++ open-layers/src/components/map/OlMap.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useRef } from 'react'
 import styles from './OlMap.module.scss'
+import 'ol/ol.css'
+import { Map, View } from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import XYZ from 'ol/source/XYZ'
 
 export default function OlMap() {
+  const mapRef = useRef<Map | null>(null)
+
+  useEffect(() => {
+    if (mapRef.current) return
+
+    const osmTileLayer = new TileLayer({
+      source: new XYZ({
+        url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      }),
+    })
+
+    const mapObject = new Map({
+      target: 'map',
+      layers: [osmTileLayer],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    })
+
+    mapRef.current = mapObject
+  }, [])
+
   return (
     <section className={styles.map__container}>
-      <div>map</div>
+      <div id='map' className={styles.map} />
     </section>
   )
 }

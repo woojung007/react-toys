2910dcd4915b59b38d5cae1265af8e8699f48777
woojung007 <eeeeve007@gmail.com>
2025-03-26 23:33:57 +0900

    ✨ 12.4) 페이지 라우팅 3. 페이지 이동

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,16 +2,32 @@ import Diary from 'pages/Diary';
 import Home from 'pages/Home';
 import New from 'pages/New';
 import Notfound from 'pages/Notfound';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
+    const navigate = useNavigate();
+
+    const onClickButton = () => {
+        navigate('/new');
+    };
+
     return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/new' element={<New />} />
-            <Route path='/diary' element={<Diary />} />
-            <Route path='/*' element={<Notfound />} />
-        </Routes>
+        <>
+            <div>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/new'}>New</Link>
+                <Link to={'/diary'}>Diary</Link>
+            </div>
+
+            <button onClick={onClickButton}>New 페이지로 이동</button>
+
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/new' element={<New />} />
+                <Route path='/diary' element={<Diary />} />
+                <Route path='/*' element={<Notfound />} />
+            </Routes>
+        </>
     );
 };
 

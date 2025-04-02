d0df40011a4d6d9153bde65cdccca9d08a58e6bb
woojung007 <eeeeve007@gmail.com>
2025-04-02 23:58:25 +0900

    ✨ 12.6) 폰트, 이미지, 레이아웃 설정하기

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,6 +3,11 @@ import Home from 'pages/Home';
 import New from 'pages/New';
 import Notfound from 'pages/Notfound';
 import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import emotion1 from 'assets/emotions/emotion1.png';
+import emotion2 from 'assets/emotions/emotion2.png';
+import emotion3 from 'assets/emotions/emotion3.png';
+import emotion4 from 'assets/emotions/emotion4.png';
+import emotion5 from 'assets/emotions/emotion5.png';
 
 const App = () => {
     const navigate = useNavigate();
@@ -13,6 +18,13 @@ const App = () => {
 
     return (
         <>
+            <div>
+                <img src={emotion1} alt='emotion1' />
+                <img src={emotion2} alt='emotion2' />
+                <img src={emotion3} alt='emotion3' />
+                <img src={emotion4} alt='emotion4' />
+                <img src={emotion5} alt='emotion5' />
+            </div>
             <div>
                 <Link to={'/'}>Home</Link>
                 <Link to={'/new'}>New</Link>
